fc83c06a95d359f65cffc795afe5294d21db4546
denniskioko <kiokodennis6990@gmail.com>
2024-01-16 17:42:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const n = array.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
-*/
+*/ 
+// The time complexity uses quadratic time complexity O(n^2), 
+//      where "n" is the length of the input array.
+// This is because the function uses nested loops iteration 
+//      to compare each pair of elements in the array,
 
 /* 
   Add your pseudocode here
+  Create a function that has two arguments(array, target)
+  create a function to find the length of the array
+  itereate over the elements of an array to find whether their sum equals to target
+          if true returns true;
+          if false return false;
 */
 
 /*
   Add written explanation of your solution here
+  // Outer loop (for (let i = 0; i < n; i++)) iterates through each element in the array
+  // The inner loop (for (let j = i + 1; j < n; j++)) iterates through elements
+      after the current element arr[i]
+  // Function checks if the sum of elements at indices i and j equals the target, returning true
+  // If no pair is found after both loops, the function returns false    
+  
 */
 
 // You can run `node index.js` to view these console logs
